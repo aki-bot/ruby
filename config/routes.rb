46256f6e8196e0feea0220999b2f56e2bceb8a8c
”ak-bot” <canacana19@icloud.com>
2020-05-12 18:19:25 +0900

    Route設定を変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,18 @@
 # 書き方..「Rails.application.routes.braw do」
 # 「get」でもらったURLを「root」でどこの「Controllerのアクション」に飛ばすか指定する
 # 最後に「end」をつけて終了
-
 Rails.application.routes.draw do
-  get 'comments/new'
-  # get 'topics/new'
   get 'sessions/new'
-  # get 'users/new'
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
-  get 'pages/help'
-  resources :users
-  resources :topics
-  
-  #コメント
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  get 'pages/index'
+  root 'pages#index'
+  get 'pages/help'
+  
+  resources :users
+  resources :topics
   get 'favorites/index'
   post '/favorites', to: 'favorites#create'
 end
